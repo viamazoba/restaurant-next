bd949ba8c06a0c8c248aeea71b43499236da92b4
Victor Mazo <viamazoba@unal.edu.co>
2024-11-26 12:04:46 -0500

    feat: Se muestra el total de la orden.

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -2,11 +2,14 @@
 
 import { useStore } from "@/src/store"
 import { ProductDetails } from "./ProductDetails"
+import { useMemo } from "react"
+import { formatCurrency } from "@/src/utils"
 
 
 export const OrderSummary = () => {
 
     const { order } = useStore()
+    const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -25,6 +28,10 @@ export const OrderSummary = () => {
                                     />
                                 ))
                             }
+                            <p className="text-2xl mt-20 text-center">
+                                Total a pagar: {' '}
+                                <span className="font-bold">{formatCurrency(total)}</span>
+                            </p>
                         </div>
                     )
             }
