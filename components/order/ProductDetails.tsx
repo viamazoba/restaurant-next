9ee4b159e4aeca83f8c91f2485d3be14ccb47c32
Victor Mazo <viamazoba@unal.edu.co>
2024-11-26 11:43:30 -0500

    feat: Se implementa añadir productos a la orden, aumentarlos y disminuirlos.

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -1,14 +1,25 @@
+import { useStore } from "@/src/store"
 import { OrderItem } from "@/src/types"
 import { formatCurrency } from "@/src/utils"
 import { PlusIcon } from "@heroicons/react/24/outline"
 import { MinusIcon } from "@heroicons/react/24/outline"
 import { XCircleIcon } from "@heroicons/react/24/outline"
+import { useMemo } from "react"
 
 type ProductDetailsProps = {
     item: OrderItem
 }
 
+
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
+
 export const ProductDetails = ({ item }: ProductDetailsProps) => {
+
+    const { increaseQuantity, decreaseQuantity } = useStore()
+    const disableDecreaseButton = useMemo(() => item.quantity === MIN_ITEMS, [item])
+    const disableIncreaseButton = useMemo(() => item.quantity === MAX_ITEMS, [item])
 
     return (
         <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
@@ -29,7 +40,9 @@ export const ProductDetails = ({ item }: ProductDetailsProps) => {
                 <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => decreaseQuantity(item.id)}
+                        disabled={disableDecreaseButton}
+                        className="disabled:opacity-20"
                     >
                         <MinusIcon className="h-6 w-6" />
                     </button>
@@ -40,7 +53,9 @@ export const ProductDetails = ({ item }: ProductDetailsProps) => {
 
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => increaseQuantity(item.id)}
+                        disabled={disableIncreaseButton}
+                        className="disabled:opacity-20"
                     >
                         <PlusIcon className="h-6 w-6" />
                     </button>
--- src/store.ts
+++ src/store.ts
@@ -6,20 +6,52 @@ import { Product } from '@prisma/client'
 interface Store {
     order: OrderItem[]
     addToOrder: (product: Product) => void
+    increaseQuantity: (id: Product['id']) => void
+    decreaseQuantity: (id: Product['id']) => void
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
     order: [],
     addToOrder: (product) => {
 
         const { categoryId, image, ...data } = product
-
-        set((state) => ({
-            order: [...state.order, {
+        let order: OrderItem[] = []
+        if (get().order.find(item => item.id === product.id)) {
+            order = get().order.map(item => item.id === product.id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        } else {
+            order = [...get().order, {
                 ...data,
                 quantity: 1,
                 subtotal: 1 * product.price
             }]
+        }
+
+        set(() => ({
+            order
+        }))
+    },
+    increaseQuantity: (id) => {
+        set((state) => ({
+            order: state.order.map(item => item.id === id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        }))
+    },
+    decreaseQuantity: (id) => {
+
+        const order = get().order.map(item => item.id === id ? {
+            ...item,
+            quantity: item.quantity - 1,
+            subtotal: item.price * (item.quantity - 1)
+        } : item)
+        set(() => ({
+            order
         }))
     }
 }))
