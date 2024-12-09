f84fc2e8b11ba0495eb1dc25c1998d5d399f613e
Victor Mazo <viamazoba@unal.edu.co>
2024-12-09 15:15:44 -0500

    feat: Se implementa paginación básica en la tabla de productos para el administrador.

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,17 +1,46 @@
+import { ProductsPagination } from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import { Heading } from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import { redirect } from "next/navigation";
 
 
-async function getProducts() {
-    const products = await prisma.product.findMany()
+async function productCount() {
+    return await prisma.product.count()
+}
+
+async function getProducts(page: number, pageSize: number) {
+    const skip = (page - 1) * pageSize
+    const products = await prisma.product.findMany({
+        take: pageSize,
+        skip,
+        include: {
+            category: true
+        }
+    })
 
     return products
 }
 
-export default async function ProductsPage() {
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 
-    const products = await getProducts()
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
+
+    const page = +searchParams.page || 1
+    const pageSize = 10
+
+    if (page < 0) redirect('/admin/products')
+
+    const productsData = getProducts(page, pageSize)
+    const totalProductsData = productCount()
+
+    const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
+
+    const totalPages = Math.ceil(totalProducts / pageSize)
+
+    if (page > totalPages) {
+        redirect('/admin/products')
+    }
 
     return (
         <>
@@ -21,6 +50,11 @@ export default async function ProductsPage() {
 
             <ProductTable
                 products={products}
+            />
+
+            <ProductsPagination
+                page={page}
+                totalPages={totalPages}
             />
         </>
     )
