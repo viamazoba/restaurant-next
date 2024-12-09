cef61fc259203d55a43b10530cfc568a8d382be8
Victor Mazo <viamazoba@unal.edu.co>
2024-12-08 23:13:21 -0500

    feat: Se finaliza la card de ordenes para el administrador.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -11,3 +11,7 @@ export const OrderSchema = z.object({
         subtotal: z.number()
     }))
 })
+
+export const OrderIdSchema = z.object({
+    orderId: z.string().transform((value) => parseInt(value)).refine(value => value > 0, { message: 'Hay errores' })
+})
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,13 @@
-import { Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from "@prisma/client";
 
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
     quantity: number
     subtotal: number
+}
+
+export type OrderWithProducts = Order & {
+    orderProducts: (OrderProducts & {
+        product: Product
+    })[]
 }
