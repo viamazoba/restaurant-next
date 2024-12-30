b2532732610ad341f453d08877a5fd0c926a7594
Victor Mazo <viamazoba@unal.edu.co>
2024-12-29 20:19:30 -0500

    feat: Se implementa revalidación automática de pedidos con SWR.

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,39 +1,33 @@
+"use client"
+import useSWR from "swr";
 import { OrderCard } from "@/components/order/OrderCard";
 import { Heading } from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-    const orders = await prisma.order.findMany({
-        where: {
-            status: false
-        },
-        include: {
-            OrderProducts: {
-                include: {
-                    product: true
-                }
-            }
-        }
+
+
+export default function OrdersPage() {
+    const url = '/admin/orders/api'
+    const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+    const { data, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval: 60000,
+        revalidateOnFocus: false
     })
 
-    return orders
-}
+    if (isLoading) return 'Cargando ...'
 
-export default async function OrdersPage() {
-
-    const orders = await getPendingOrders()
-
-    return (
+    if (data) return (
         <>
             <Heading>
                 Administrar Ordenes
             </Heading>
 
+
             {
-                orders.length ? (
+                data.length ? (
                     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
                         {
-                            orders.map(order => (
+                            data.map(order => (
                                 <OrderCard
                                     key={order.id}
                                     order={order}
